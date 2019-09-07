be9fe44f5f8cf1a3a890d16a93b1879be10f7834
jimmyjhickey <jimmyjhickey@gmail.com>
2019-09-07 16:06:58 -0400

    Added card combinatorics script

--- st701-statistical-theory-1/card_combinatorics.R
+++ st701-statistical-theory-1/card_combinatorics.R
@@ -6,13 +6,13 @@
 
 # P(Full House | 2 ranks)
 
-P_Full_given_ranks = (13 * choose(4,3) * 12 * choose(4,2)) / 
-                      (13 * choose(4,3) * 12 * choose(4,2) + 13 * choose(4,4) * 12 * choose(4,1) )
+P_Full_given_ranks = (2 * choose(4,3) * 1 * choose(4,2)) / 
+                      ( choose(13, 2) * choose(8,5) )
 P_Full_given_ranks
-# 0.8571429
+# 0.010989
 
 # P(Full House | all face cards & 3 suits)
-P_Full_give_face_3suits = ( 3 * choose(3,3) * 2 * choose(3,2) ) / (choose(9,5))
+P_Full_give_face_3suits = ( 3 * choose(3,3) * 2 * choose(3,2) ) / (choose(9,5) * choose(4,3))
 P_Full_give_face_3suits
 
 # P(Full House | all # cards (A-10))
