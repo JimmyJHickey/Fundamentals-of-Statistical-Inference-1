0a69336e129ab30ce17fe310e0b31ed31fed4f5d
jimmyjhickey <jimmyjhickey@gmail.com>
2020-06-30 12:42:38 -0400

    Added comments

--- misc/helping_anna/LoopingFunction.R
+++ misc/helping_anna/LoopingFunction.R
@@ -1,12 +1,19 @@
 # Jimmy code
 practice <- read.csv("TestData.csv", header = TRUE)
 
+# helpful library
 library(dplyr)
 
+# first column had weird symbols in name
 names(practice)[1] = "IDvar"
 
+# group practice by column "IDvar",
+# then the summarize command will work across groups
+# n() gives count of each group
+# sum(ColName) gives the sum of the elements in that column for that group
 output = practice %>%
   group_by(IDvar) %>%
   summarize(count = n(), test1_total = sum(Test1), test2_total = sum(Test2), )
 
+# row.names = FALSE gets rid of extra column
 write.csv(output, "summarized_data.csv", row.names = FALSE)
