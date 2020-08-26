2c047e3158a8d4a3009823eb0cf075800466d79a
jimmyjhickey <jimmyjhickey@gmail.com>
2020-08-25 21:23:23 -0400

    Started R file for hw1

---
+++ st790-dynamic-treatment-regimes/homework1/homework1.R
@@ -0,0 +1,8 @@
+###
+# Jimmy Hickey
+# 2020-08-24
+# Introduction to Dynamic Treatment Regimes
+# Data analysis homework 1
+###
+
+# http://www.laber-labs.com/dtr-book/Chapter2/treatment_effect.html
