317bb1a98e22985ca6bb66287a9600084e9e7c3f
jimmyjhickey <jimmyjhickey@gmail.com>
2020-09-10 21:22:47 -0400

    added lecture notes

---
+++ st790-dynamic-treatment-regimes/dyntx_lecture.R
@@ -0,0 +1,23 @@
+###
+# Jimmy Hickey
+# 2020-09-10
+# Lecture by Shannon Holloway
+###
+
+library(modelObj)
+library(DynTxRegime)
+
+df = read.csv("data/dyntx_lecture.txt")
+summary(df)
+
+moMain <- buildModelObj(model = ~x1+x2,
+                        solver.method = 'lm',
+                        predict.method = 'predict.lm')
+
+moCont <- buildModelObj(mode = ~x2+x3,
+                        solver.method = 'lm',
+                        predict.method = 'predict.lm')
+
+qObj <- qLearn(moMain = moMain, moCont = moCont, iter = 0L,
+               data = df, response = df$y, txName = 'A',
+               verbose = TRUE)
